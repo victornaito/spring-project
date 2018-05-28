7f69b13f0ea2e08d10971a9f2e1dc5c542a5b983
Naito <victor.naito@redetendencia.com.br>
2018-05-28 14:15:04 -0400

    Empresa COntroller

--- demo/src/main/java/com/naito/api/controller/EmpresaController.java
+++ demo/src/main/java/com/naito/api/controller/EmpresaController.java
@@ -1,24 +1,24 @@
-package controller;
+package com.naito.api.controller;
+
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.naito.api.repository.EmpresaRepository;
-
-import antlr.collections.List;
+import com.naito.api.service.EmpresaService;
 
 @RestController
 @RequestMapping("/api/empresas")
 public class EmpresaController<Empresa> {
 	
 	@Autowired
-	EmpresaRepository empresaRepository;
+	EmpresaService empresaService;
 	
 	@GetMapping("listar")
-    public List obterEmpresa() {
-		return (List) this.empresaRepository.findAll();
+    public  ArrayList<Empresa> obterEmpresa() {
+		return (ArrayList<Empresa>) this.empresaService.obterEmpresa();
 	}
 
 }
